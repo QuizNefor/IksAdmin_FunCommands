3a501caaf1cc259b1a6ec61d2e930cec8c2e4397
Quiz <kirizo@bk.ru>
2024-04-27 01:33:07 +0700

    new file:   .idea/.idea.IksAdmin_FunCommands.dir/.idea/.gitignore new file:   .idea/.idea.IksAdmin_FunCommands.dir/.idea/indexLayout.xml new file:   Extentions/PlayerExtensions.cs new file:   IksAdmin_FunCommands.csproj new file:   IksAdmin_FunCommands.sln new file:   Main.cs new file:   README.md new file:   XHelper.cs new file:   obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs new file:   obj/Debug/net8.0/IksAdmin.5E5E57B3.Up2Date new file:   obj/Debug/net8.0/IksAdmin_FunCommands.AssemblyInfo.cs new file:   obj/Debug/net8.0/IksAdmin_FunCommands.AssemblyInfoInputs.cache new file:   obj/Debug/net8.0/IksAdmin_FunCommands.GeneratedMSBuildEditorConfig.editorconfig new file:   obj/Debug/net8.0/IksAdmin_FunCommands.GlobalUsings.g.cs new file:   obj/Debug/net8.0/IksAdmin_FunCommands.assets.cache new file:   obj/Debug/net8.0/IksAdmin_FunCommands.csproj.AssemblyReference.cache new file:   obj/Debug/net8.0/IksAdmin_FunCommands.csproj.CoreCompileInputs.cache new file:   obj/Debug/net8.0/IksAdmin_FunCommands.csproj.FileListAbsolute.txt new file:   obj/Debug/net8.0/IksAdmin_FunCommands.dll new file:   obj/Debug/net8.0/IksAdmin_FunCommands.pdb new file:   obj/Debug/net8.0/ref/IksAdmin_FunCommands.dll new file:   obj/Debug/net8.0/refint/IksAdmin_FunCommands.dll new file:   obj/IksAdmin_FunCommands.csproj.nuget.dgspec.json new file:   obj/IksAdmin_FunCommands.csproj.nuget.g.props new file:   obj/IksAdmin_FunCommands.csproj.nuget.g.targets new file:   obj/project.assets.json new file:   obj/project.nuget.cache new file:   obj/project.packagespec.json new file:   obj/rider.project.restore.info

---
+++ Main.cs
@@ -0,0 +1,357 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Capabilities;
+using CounterStrikeSharp.API.Modules.Commands;
+using IksAdminApi;
+
+namespace IksAdmin_FunCommands;
+
+public class Main : BasePlugin
+{
+    public override string ModuleName => "IksAdmin_FunCommands";
+    public override string ModuleVersion => "1.0.0";
+    private static PluginCapability<IIksAdminApi> _adminApiCapability = new("iksadmin:core");
+    public IIksAdminApi? AdminApi;
+
+    public override void OnAllPluginsLoaded(bool hotReload)
+    {
+        AdminApi = _adminApiCapability.Get();
+        AdminApi!.AddNewCommand(
+            "clip_set",
+            "Set ammo count",
+            "css_clip_set <#UID/#SID/name> <ammo count>",
+            2,
+            "clip_set",
+            "b",
+            CommandUsage.CLIENT_AND_SERVER,
+            OnClipSetCommand
+            );
+        AdminApi!.AddNewCommand(
+            "slap",
+            "Slap player",
+            "css_slap <#UID/#SID/name> <damage>",
+            2,
+            "slap",
+            "b",
+            CommandUsage.CLIENT_AND_SERVER,
+            OnSlapCommand
+            );
+        AdminApi!.AddNewCommand(
+                "speed",
+            "Set player move speed",
+            "css_speed <#UID/#SID/name> <speed>",
+            2,
+            "speed",
+            "b",
+            CommandUsage.CLIENT_AND_SERVER,
+            OnSpeedSetCommand
+            );
+        AdminApi!.AddNewCommand(
+            "gravity",
+            "Set player gravity",
+            "css_gravity <#UID/#SID/name> <gravity>",
+            2,
+            "gravity",
+            "b",
+            CommandUsage.CLIENT_AND_SERVER,
+            OnGravityCommand
+        );
+        AdminApi!.AddNewCommand(
+            "set_money",
+            "Set player money",
+            "css_set_money <#UID/#SID/name> <money>",
+            2,
+            "set_money",
+            "b",
+            CommandUsage.CLIENT_AND_SERVER,
+            OnSetMoneyCommand
+        );
+        AdminApi!.AddNewCommand(
+            "hp",
+            "Set player hp",
+            "css_hp <#UID/#SID/name> <hp>",
+            2,
+            "hp",
+            "b",
+            CommandUsage.CLIENT_AND_SERVER,
+            OnHpCommand
+        );
+        AdminApi!.AddNewCommand(
+            "bury",
+            "Bury player",
+            "css_bury <#UID/#SID/name> <depth (default = 10)>",
+            2,
+            "bury",
+            "b",
+            CommandUsage.CLIENT_AND_SERVER,
+            OnBuryCommand
+        );
+        AdminApi!.AddNewCommand(
+            "unbury",
+            "Unbury player",
+            "css_unbury <#UID/#SID/name> <depth (default = 15)>",
+            2,
+            "unbury",
+            "b",
+            CommandUsage.CLIENT_AND_SERVER,
+            OnUnBuryCommand
+        );
+        AdminApi!.AddNewCommand(
+            "freeze",
+            "Freeze player",
+            "css_freeze <#UID/#SID/name>",
+            1,
+            "freeze",
+            "b",
+            CommandUsage.CLIENT_AND_SERVER,
+            OnFreezeCommand
+        );
+        AdminApi!.AddNewCommand(
+            "unfreeze",
+            "Unfreeze player",
+            "css_unfreeze <#UID/#SID/name>",
+            1,
+            "unfreeze",
+            "b",
+            CommandUsage.CLIENT_AND_SERVER,
+            OnUnFreezeCommand
+        );
+        AdminApi!.AddNewCommand(
+            "noclip",
+            "Noclip for player",
+            "css_noclip <#UID/#SID/name>",
+            1,
+            "noclip",
+            "b",
+            CommandUsage.CLIENT_AND_SERVER,
+            OnNoclipCommand
+        );
+        AdminApi!.AddNewCommand(
+            "teleportplayer",
+            "Teleport to player",
+            "css_teleportplayer <#UID/#SID/name> <#UID/#SID/name>",
+            2,
+            "teleportplayer",
+            "b",
+            CommandUsage.CLIENT_AND_SERVER,
+            OnTeleportPlayerCommand
+        );
+    }
+
+    private void OnTeleportPlayerCommand(CCSPlayerController caller, Admin? admin, List<string> args, CommandInfo info)
+    {
+        var target = XHelper.GetPlayerFromArg(args[0]);
+        if (target == null)
+        {
+            AdminApi.SendMessageToPlayer(caller, AdminApi.Localizer["NOTIFY_PlayerNotFound"]);
+            return;
+        }
+        if (!target.PawnIsAlive)
+        {
+            AdminApi.SendMessageToPlayer(caller, Localizer["ERROR_PlayerNotAlive"]);
+            return;
+        }
+        var target1 = XHelper.GetPlayerFromArg(args[0]);
+        if (target1 == null)
+        {
+            AdminApi.SendMessageToPlayer(caller, AdminApi.Localizer["NOTIFY_PlayerNotFound"]);
+            return;
+        }
+        if (!target1.PawnIsAlive)
+        {
+            AdminApi.SendMessageToPlayer(caller, Localizer["ERROR_PlayerNotAlive"]);
+            return;
+        }
+        target.TeleportPlayer(target1);
+    }
+
+    private void OnNoclipCommand(CCSPlayerController caller, Admin? admin, List<string> args, CommandInfo info)
+    {
+        var target = XHelper.GetPlayerFromArg(args[0]);
+        if (target == null)
+        {
+            AdminApi.SendMessageToPlayer(caller, AdminApi.Localizer["NOTIFY_PlayerNotFound"]);
+            return;
+        }
+        if (!target.PawnIsAlive)
+        {
+            AdminApi.SendMessageToPlayer(caller, Localizer["ERROR_PlayerNotAlive"]);
+            return;
+        }
+        target.PlayerPawn.Value!.ToggleNoclip();
+    }
+
+
+    private void OnUnFreezeCommand(CCSPlayerController caller, Admin? admin, List<string> args, CommandInfo info)
+    {
+        var target = XHelper.GetPlayerFromArg(args[0]);
+        if (target == null)
+        {
+            AdminApi.SendMessageToPlayer(caller, AdminApi.Localizer["NOTIFY_PlayerNotFound"]);
+            return;
+        }
+        if (!target.PawnIsAlive)
+        {
+            AdminApi.SendMessageToPlayer(caller, Localizer["ERROR_PlayerNotAlive"]);
+            return;
+        }
+        target.PlayerPawn.Value!.Unfreeze();
+    }
+
+    private void OnFreezeCommand(CCSPlayerController caller, Admin? admin, List<string> args, CommandInfo info)
+    {
+        var target = XHelper.GetPlayerFromArg(args[0]);
+        if (target == null)
+        {
+            AdminApi.SendMessageToPlayer(caller, AdminApi.Localizer["NOTIFY_PlayerNotFound"]);
+            return;
+        }
+        if (!target.PawnIsAlive)
+        {
+            AdminApi.SendMessageToPlayer(caller, Localizer["ERROR_PlayerNotAlive"]);
+            return;
+        }
+        target.PlayerPawn.Value!.Freeze();
+    }
+
+    private void OnUnBuryCommand(CCSPlayerController caller, Admin? admin, List<string> args, CommandInfo info)
+    {
+        var target = XHelper.GetPlayerFromArg(args[0]);
+        if (target == null)
+        {
+            AdminApi.SendMessageToPlayer(caller, AdminApi.Localizer["NOTIFY_PlayerNotFound"]);
+            return;
+        }
+        if (!target.PawnIsAlive)
+        {
+            AdminApi.SendMessageToPlayer(caller, Localizer["ERROR_PlayerNotAlive"]);
+            return;
+        }
+        target.PlayerPawn.Value!.Unbury(int.Parse(args[1]));
+    }
+
+    private void OnBuryCommand(CCSPlayerController caller, Admin? admin, List<string> args, CommandInfo info)
+    {
+        var target = XHelper.GetPlayerFromArg(args[0]);
+        if (target == null)
+        {
+            AdminApi.SendMessageToPlayer(caller, AdminApi.Localizer["NOTIFY_PlayerNotFound"]);
+            return;
+        }
+        if (!target.PawnIsAlive)
+        {
+            AdminApi.SendMessageToPlayer(caller, Localizer["ERROR_PlayerNotAlive"]);
+            return;
+        }
+        target.PlayerPawn.Value!.Bury(int.Parse(args[1]));
+    }
+
+    private void OnHpCommand(CCSPlayerController caller, Admin? admin, List<string> args, CommandInfo info)
+    {
+        var target = XHelper.GetPlayerFromArg(args[0]);
+        if (target == null)
+        {
+            AdminApi.SendMessageToPlayer(caller, AdminApi.Localizer["NOTIFY_PlayerNotFound"]);
+            return;
+        }
+        if (!target.PawnIsAlive)
+        {
+            AdminApi.SendMessageToPlayer(caller, Localizer["ERROR_PlayerNotAlive"]);
+            return;
+        }
+        target.SetHp(int.Parse(args[1]));
+    }
+
+    private void OnSetMoneyCommand(CCSPlayerController caller, Admin? admin, List<string> args, CommandInfo info)
+    {
+        var target = XHelper.GetPlayerFromArg(args[0]);
+        if (target == null)
+        {
+            AdminApi.SendMessageToPlayer(caller, AdminApi.Localizer["NOTIFY_PlayerNotFound"]);
+            return;
+        }
+        if (!target.PawnIsAlive)
+        {
+            AdminApi.SendMessageToPlayer(caller, Localizer["ERROR_PlayerNotAlive"]);
+            return;
+        }
+        target.SetMoney(int.Parse(args[1]));
+    }
+
+    private void OnGravityCommand(CCSPlayerController caller, Admin? admin, List<string> args, CommandInfo info)
+    {
+        var target = XHelper.GetPlayerFromArg(args[0]);
+        if (target == null)
+        {
+            AdminApi.SendMessageToPlayer(caller, AdminApi.Localizer["NOTIFY_PlayerNotFound"]);
+            return;
+        }
+        if (!target.PawnIsAlive)
+        {
+            AdminApi.SendMessageToPlayer(caller, Localizer["ERROR_PlayerNotAlive"]);
+            return;
+        }
+        target.SetGravity(int.Parse(args[1]));
+    }
+
+    private void OnSpeedSetCommand(CCSPlayerController caller, Admin? admin, List<string> args, CommandInfo info)
+    {
+        var target = XHelper.GetPlayerFromArg(args[0]);
+        if (target == null)
+        {
+            AdminApi.SendMessageToPlayer(caller, AdminApi.Localizer["NOTIFY_PlayerNotFound"]);
+            return;
+        }
+        if (!target.PawnIsAlive)
+        {
+            AdminApi.SendMessageToPlayer(caller, Localizer["ERROR_PlayerNotAlive"]);
+            return;
+        }
+        target.SetSpeed(int.Parse(args[1]));
+    }
+
+    private void OnSlapCommand(CCSPlayerController caller, Admin? admin, List<string> args, CommandInfo info)
+    {
+        var target = XHelper.GetPlayerFromArg(args[0]);
+        if (target == null)
+        {
+            AdminApi.SendMessageToPlayer(caller, AdminApi.Localizer["NOTIFY_PlayerNotFound"]);
+            return;
+        }
+        if (!target.PawnIsAlive)
+        {
+            AdminApi.SendMessageToPlayer(caller, Localizer["ERROR_PlayerNotAlive"]);
+            return;
+        }
+
+        target.PlayerPawn.Value!.Slap(int.Parse(args[1]));
+    }
+
+    private void OnClipSetCommand(CCSPlayerController caller, Admin? admin, List<string> args, CommandInfo info)
+    {
+        var target = XHelper.GetPlayerFromArg(args[0]);
+        if (target == null)
+        {
+            AdminApi.SendMessageToPlayer(caller, AdminApi.Localizer["NOTIFY_PlayerNotFound"]);
+            return;
+        }
+        if (!target.PawnIsAlive)
+        {
+            AdminApi.SendMessageToPlayer(caller, Localizer["ERROR_PlayerNotAlive"]);
+            return;
+        }
+        var weapon = target.PlayerPawn.Value!.WeaponServices!.ActiveWeapon;
+        weapon.Value!.Clip1 = int.Parse(args[0]);
+        weapon.Value!.VData!.MaxClip1 = int.Parse(args[0]);
+        Utilities.SetStateChanged(target.PlayerPawn.Value, "CBasePlayerPawn", "m_pWeaponServices");
+        
+        
+        
+        
+        
+        
+        
+    }
+}   
+    
---
+++ README.md
@@ -0,0 +1,26 @@
+## IksAdmin_FunCommands
+
+
+
+
+
+## Features
+- Module
+- Fun
+- Customizable translations
+
+
+
+## Commands
+- `css_clip_set <#UID/#SID/name> <ammo count>` | Flags access: `"clip_set"` Default: `b`
+- `css_slap <#UID/#SID/name> <damage>` | Flags access: `"slap"` Default: `b`
+- `css_speed <#UID/#SID/name> <speed>` | Flags access: `"speed"` Default: `b`
+- `css_gravity <#UID/#SID/name> <gravity>` | Flags access: `"gravity"` Default: `b`
+- `css_set_money <#UID/#SID/name> <money>` | Flags access: `"money"` Default: `b`
+- `css_hp <#UID/#SID/name> <hp>` | Flags access: `"hp"` Default: `b`
+- `css_bury <#UID/#SID/name> <depth (default = 10)>` | Flags access: `"bury"` Default: `b`
+- `css_unbury <#UID/#SID/name> <depth (default = 15)>` | Flags access: `"unbury"` Default: `b`
+- `css_freeze <#UID/#SID/name>` | Flags access: `"freeze"` Default: `b`
+- `css_unfreeze <#UID/#SID/name>` | Flags access: `"unfreeze"` Default: `b`
+- `css_noclip <#UID/#SID/name>` | Flags access: `"noclip"` Default: `b`
+- `css_teleportplayer <#UID/#SID/name> <#UID/#SID/name>` | Flags access: `teleportplayer"` Default: `b`
